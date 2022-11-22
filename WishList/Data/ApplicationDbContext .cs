368f6e666694a57ebd199da6db9122fc0c026eee
Martin Bay Thomsen <MABAY@nuuday.dk>
2022-11-22 10:54:08 +0100

    Completed module

---
+++ WishList/Data/ApplicationDbContext .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext( Microsoft.EntityFrameworkCore.DbContextOptions options) : base(options)
+        { }
+    }
+}
