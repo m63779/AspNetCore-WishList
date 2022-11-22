0f76e2b036d4fd2c15c114e382509da987644597
Martin Bay Thomsen <MABAY@nuuday.dk>
2022-11-22 12:57:58 +0100

    Completed module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        
+
+        private readonly ApplicationDbContext _context { get; set; }
+
+        public ItemController(ApplicationDbContext applicationDbContext) {
+            _context = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(List<_context.Items>);
+        }
+    }
+}
