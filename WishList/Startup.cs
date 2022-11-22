1aca99b19addde5ed5eabe1df8ca9a60573e7cc4
Martin Bay Thomsen <MABAY@nuuday.dk>
2022-11-22 10:12:02 +0100

    Completed module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -28,7 +28,7 @@ namespace WishList
                 app.UseDeveloperExceptionPage();
             } 
             else {
-                app.UseExceptionHandler("/Home/error");
+                app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
             app.UseEndpoints( endpoints => { endpoints.MapDefaultControllerRoute(); });
