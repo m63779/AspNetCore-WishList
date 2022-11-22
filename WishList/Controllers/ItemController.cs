12f290b9e387a52539d980ae85318b045c13995b
Martin Bay Thomsen <MABAY@nuuday.dk>
2022-11-22 14:27:41 +0100

    Completed module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -9,15 +9,39 @@ namespace WishList.Controllers
 {
     public class ItemController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        private ApplicationDbContext Context { get; set; }
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
 
-        public ItemController(ApplicationDbContext applicationDbContext) {
-            Context = applicationDbContext;
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create( Models.Item item )
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
